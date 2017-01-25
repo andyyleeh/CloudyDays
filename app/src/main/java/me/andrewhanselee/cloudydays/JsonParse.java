278010c7e7c01958ca8d6676849afaa59d706e5b
Han Se (Andrew) Lee <hs27lee@edu.uwaterloo.ca>
2017-01-25 02:05:51 -0500

    more info displayed

--- app/src/main/java/me/andrewhanselee/cloudydays/JsonParse.java
+++ app/src/main/java/me/andrewhanselee/cloudydays/JsonParse.java
@@ -6,6 +6,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import me.andrewhanselee.cloudydays.NumberFormatting.TempFormat;
+
 /**
  * Created by Andrew on 2017-01-19.
  */
@@ -34,6 +36,40 @@ public class JsonParse {
 
 
         parsed[0] = forecastCity + "-" + description;
+
+
+        return parsed;
+    }
+
+    public static String parsedDay (Context context, String JsonString) throws JSONException{
+        final String CITY = "name";
+        String parsed = null;
+
+        JSONObject forecast = new JSONObject(JsonString);
+        JSONArray forecastArray = forecast.getJSONArray("list");
+
+        JSONObject cityInfo= forecast.getJSONObject("city");
+
+        String forecastCity = cityInfo.getString(CITY);
+
+        String description;
+
+        JSONObject today = forecastArray.getJSONObject(0);
+        JSONArray weather = today.getJSONArray("weather");
+        JSONObject main = today.getJSONObject("main");
+        double currentT = main.getDouble("temp");
+        String now = TempFormat.formatTemp(context, currentT);
+        double max = main.getDouble("temp_max");
+        double min = main.getDouble("temp_min");
+        String hL = TempFormat.formatHL(context, max, min);
+
+
+        JSONObject obj = weather.getJSONObject(0);
+        description = obj.getString("description");
+
+
+
+        parsed = forecastCity + "-" + description + "\n" + now + " \n" + hL;
 
 
         return parsed;
